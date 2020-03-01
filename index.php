f9a1f8cbf9d5de4384b21366514d2fd013ea63ed
Ivan Sambrana <sambranaivan@gmail.com>
2020-03-01 15:30:35 -0300

    whoami

--- index.php
+++ index.php
@@ -1,7 +1,10 @@
 <!DOCTYPE html>
 <html>
 <body>
+<?php
 
+echo exec('whoami');
+?>
 <form action="upload.php" method ="post" enctype="multipart/form-data">
     <input type="file" name="file" />
     <input type="submit" value="submit" name="submit"/>

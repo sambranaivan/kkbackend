cf60782c3e2149eac936e5ed75d9ab0532543b69
Ivan Sambrana <sambranaivan@gmail.com>
2020-03-01 20:11:34 -0300

    vertical

--- upload.php
+++ upload.php
@@ -5,7 +5,7 @@ error_reporting(E_ALL);
 
   if(move_uploaded_file($temp,"uploads/".$name)){
    echo "Your file was uploaded";
-   echo exec('ffmpeg -i "uploads/video.mp4" -i "uploads/arrebol.mp4" -filter_complex "[0:v]scale=480:640,setsar=1[l];[1:v]scale=480:640,setsar=1[r];[l][r]hstack;[0][1]amix"  new.mp4');
+   echo exec('ffmpeg -i "uploads/video.mp4" -i "uploads/arrebol.mp4" -filter_complex "[0:v]scale=480:640,setsar=1[l];[1:v]scale=480:640,setsar=1[r];[l][r]vstack;[0][1]amix"  new.mp4');
 }
  else
 { 
